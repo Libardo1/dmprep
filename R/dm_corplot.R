d262418a513d9d66510ca7d08fc7e951018fc3a3
Ben R Fitzpatrick <ben.r.fitzpatrick@gmail.com>
2016-05-10 09:06:13 +1000

    idea for function to plot correlations between the columns of a design matrix

---
+++ R/dm_corplot.R
@@ -0,0 +1,11 @@
+
+
+# function to produce plots to depict the strengths of correlations between all possible pairs of covariates in a design matrix:
+
+# raster plot is the easy option
+
+# chord diagram is harder - however chord diagram easier to interpret
+
+# option to switch between abs cor and cor
+
+# mpccm cor scale cut off
