6e94e93e4ef605dc568cb5e6f0e3484392461bfc
Ben R Fitzpatrick <ben.r.fitzpatrick@gmail.com>
2016-05-09 11:38:13 +1000

    added boiler plate message and roxygen2 documentation

--- R/dm_expand.R
+++ R/dm_expand.R
@@ -1,13 +1,36 @@
-# aiapt = Add Interaction And Polynomial Terms
+# Design Matrix Expansion Function from the Design Matrix Preparation Package
+#    Copyright (C) 2016  Benjamin R. Fitzpatrick
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+# The package author may be contacted via electronic mail at <ben.r.fitzpatrick@gmail.com>
 
-# note this doesn't recenter and rescale created polynomial and interaction terms either your modelling function will have to do that or you should do it yourself before supplying the output of this function to your modelling function
+#' Expand a design matrix to include pairwise interactions terms and polynomial terms.
+#'
+#' \code{dm_expand} expands the supplied design matrix of linear main effects terms for covariates to include columns for all possible pairwise interactions of covariates and columns for polynomial terms for all covariates up to the polynomial order specified to \code{mpo}.  The additional columns for interaction terms are named following the convention where the column named 'first.covariate.name_X_second.covariate.name' contains the products of the observations in 'first.covariate.name' and 'second.covariate.name'.  The additional columns for polynomial terms are named following the convention where the polynomial order is appended to the name of the consituent covariate such that the column named 'covariate.name.2' contains the values for the quadratic (polynomial order 2) terms for the covariate called 'covariate.name'. Note: to avoid one of the consitutent covariates dominating the values of the interaction term all linear main effects terms are recentered to have means of zero and L1 norms of one prior to calculating the pairwise interactions.  Similarly to avoid extremeley large magnitude values in the column for higher order polynomial terms these are also calculate from linear main effects terms that have been recentered to have means of zero and L1 norms of one prior to calculating the pairwise interactions.
+#'
+#' @param LME The design matrix, each row is an observation, each column is a covariate.  The covariates should include linear main effects only.
+#' @param mpo the maximum polynomial order to include.
+#' @return The expanded design matrix.
+#'
+#' @examples
+#' # Expand the design matrix DM: 
+#' X.P4I2 <- dm_expand(LME = DM, mpo = 4)
 
 dm_expand <- function(LME = DM,
                   mpo = 4){
-                   
-  # LME = Linear Main Effects design matrix (as dataframe)
   # this function will only create interactions of linear main effects
-  mpo = 4 # note the facility to alter this value would require editing of polynomial term filtering code below (currently a value of 4 is hard coded in) e.g. |2|3|4
   if( (max(abs((colMeans(LME)))) < 1e-10) | (max(colSums(LME^2)) > 1) ){
     print('You have not recentred and rescaled your design matrix prior to requesting the calculation of polynomial and interactions terms. I\'ll do this for you now, recentring to column means of zero and column L1 norms of 1.')
     X <- LME
@@ -30,7 +53,8 @@ dm_expand <- function(LME = DM,
     PDM[,paste(P.ng[i,'Var'], P.ng[i,'P'], sep = '.')] = (Data[,paste(P.ng[i,'Var'])])^(as.numeric(paste(P.ng[i,'P'])))
   }
   # Expand Design Matrix to include Columns for Interactions to Order = 1/2 Max Polynomial Order
-  # Order 2 Interactions: Products of two distinct linear terms
+  # Currrently all that is implemented is the facility to include Order 2 Interactions:
+  # Products of two distinct linear terms
   IO2.2L.i = t(combn(x = 1:n.LME, m = 2))
   IO2.2L.n = character(nrow(IO2.2L.i))
   IO2.2L.DM = data.frame(matrix(0, nrow = nrow(Data), ncol = length(IO2.2L.n)))
