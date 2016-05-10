29f386ca219568ce98d74886f0c03366635450fd
Ben R Fitzpatrick <ben.r.fitzpatrick@gmail.com>
2016-05-10 15:03:44 +1000

    commented out ideas code for now so i can still load complete drafts of other functions with devtools::load_all

--- R/dm_colrm.R
+++ R/dm_colrm.R
@@ -1,10 +1,10 @@
 
 # test data:
-Test.Mat <- data.frame(matrix(data = c(1:4,NA,6:10,11:14,NA,16:20,rep(0,5),rep(NA,5)), nrow = 5))
+# Test.Mat <- data.frame(matrix(data = c(1:4,NA,6:10,11:14,NA,16:20,rep(0,5),rep(NA,5)), nrow = 5))
 
 # remove columns that contain only NA values:
 
-Test.Mat[,!apply(X = Test.Mat, MARGIN = 2, FUN = anyNA)]
+# Test.Mat[,!apply(X = Test.Mat, MARGIN = 2, FUN = anyNA)]
 
 
 # remove columns that contain entries of exactly 0 in all rows:
@@ -30,4 +30,4 @@ all.zero <- function(x){
 
 
 
-Test.Mat[,!apply(X = Test.Mat, MARGIN = 2, FUN = all.zero)]
+# Test.Mat[,!apply(X = Test.Mat, MARGIN = 2, FUN = all.zero)]
--- R/dm_corplot.R
+++ R/dm_corplot.R
@@ -7,19 +7,19 @@
 # option to turn covariate labels on or off (if you do lots of covariates small enough font on all labels to fit along axis will not be legible)
 
 
-dm_corplot(
+# dm_corplot(
 
-DM = X.5km.WS.LSO.F$FDM
+# DM = X.5km.WS.LSO.F$FDM
 
-cmat <- data.frame(abs(cor(DM, use = 'complete.obs')))
-cmat$Var1 <- row.names(cmat)
-C.df <- gather(data = cmat, value = Cor, key = Var2, -Var1)
+# cmat <- data.frame(abs(cor(DM, use = 'complete.obs')))
+# cmat$Var1 <- row.names(cmat)
+# C.df <- gather(data = cmat, value = Cor, key = Var2, -Var1)
 
-nrow(C.df)    
+# nrow(C.df)    
 
-p <- ggplot(aes(x = Var1, y = Var2, fill = Cor), data = C.df)
+# p <- ggplot(aes(x = Var1, y = Var2, fill = Cor), data = C.df)
 
-p + geom_raster() + coord_equal() + scale_fill_distiller(palette = 'Spectral', limits = c(0,0.8), na.value = 'white') + theme(axis.text.x = element_text(angle = 90, hjust = 1, vjust = 0.5), axis.text.y = element_text(vjust = 0.5), panel.background = element_rect(fill = 'white'), legend.key.height = unit(x = 4, units = 'cm')) + labs(x = '', y = '')
+# p + geom_raster() + coord_equal() + scale_fill_distiller(palette = 'Spectral', limits = c(0,0.8), na.value = 'white') + theme(axis.text.x = element_text(angle = 90, hjust = 1, vjust = 0.5), axis.text.y = element_text(vjust = 0.5), panel.background = element_rect(fill = 'white'), legend.key.height = unit(x = 4, units = 'cm')) + labs(x = '', y = '')
 
 # chord diagram is harder - however chord diagram easier to interpret
 
